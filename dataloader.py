17d67d912509e49b329945a8b08e715a5e0b5e8c
Natsagdorj-Choijilsuren <choi@mm.inf.uec.ac.jp>
2021-01-03 12:41:25 +0900

    train debugging

--- dataloader.py
+++ dataloader.py
@@ -9,7 +9,6 @@ transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize(mean= [0.485, 0.456, 0.406],
                                                      std=[0.229, 0.224, 0.225])])
 
-transform = transforms.ToTensor()
 
 categories = {
     'road': [128, 64, 128],
